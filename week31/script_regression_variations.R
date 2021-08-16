2eb92684b94b3731cfa59302fa661784957c2ffb
Neel <neelsura12@gmail.com>
2021-08-16 13:21:27 +0200

    update

--- week31/script_regression_variations.R
+++ week31/script_regression_variations.R
@@ -96,6 +96,56 @@ hist(rnorm(N, 0, 1), main=c())
 lamw <- rLambertW(N, "normal", theta=list(beta=c(0,1), delta=c(0,1/3), gamma=0, alpha=1))
 hist(lamw, main=c())
 
+# Bijective transform  --------------------------------------------------------
+
+par(mfrow = c(1, 2))
+par(cex = 0.6)
+par(mar = c(0, 0, 0, 0), oma = c(4, 4, 0.5, 0.5))
+par(tcl = -0.25)
+par(mgp = c(2, 0.6, 0))
+par(pch = 20)
+
+delta <- 1/3
+
+lamw_transform_h <- function(x, delta) {
+    y <- x * exp(delta/2 * x^2)
+}
+lamw_untransform_h <- function(y, delta) {
+    delta <- max(delta, 1e-16)
+    x <- sign(y) * sqrt(W(delta * y^2) / delta)
+}
+
+lamw_transform_hh <- function(x, delta_left, delta_right) {
+    y <- ifelse(x <= 0, 
+                x * exp(delta_left/2 * x^2),
+                x * exp(delta_right/2 * x^2))
+}
+lamw_untransform_hh <- function(x, delta_left, delta_right) {
+    delta_left <- max(delta_left, 1e-16)
+    delta_right <- max(delta_right, 1e-16)
+    x <- ifelse(y <= 0, 
+                sign(y) * sqrt(W(delta_left * y^2) / delta_left), 
+                sign(y) * sqrt(W(delta_right * y^2) / delta_right))
+}
+
+x <- seq(-4, 4, 0.01)
+y <- lamw_transform_h(x, delta)
+plot(x,y,ylim=c(-40,40))
+lines(x,x)
+mtext('h transform, delta=1/3', side = 3, line = -1, adj = 0.025, padj = 1, cex = 1, col = "grey40")
+
+y_inv <- lamw_untransform_h(y, delta)
+all(abs(y_inv - x) < 1e-8)
+
+x <- seq(-4, 4, 0.01)
+y <- lamw_transform_hh(x, delta_left, delta_right)
+plot(x,y,ylim=c(-40,40), yaxt='n')
+lines(x,x)
+mtext('hh transform, delta=(1/3, 0)', side = 3, line = -1, adj = 0.025, padj = 1, cex = 1, col = "grey40")
+
+y_inv <- lamw_untransform_hh(y, delta_left, delta_right)
+all(abs(y_inv - x) < 1e-8)
+
 # Generated samples  ----------------------------------------------------------
 
 quantile(as_draws_df(mod_out$draws())$new_y, c(0.05,0.25,0.5,0.75,0.95))
